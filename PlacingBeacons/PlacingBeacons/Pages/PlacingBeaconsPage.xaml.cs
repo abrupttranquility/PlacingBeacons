e4fdea0818e279359bf94a6babf2c751d8509b2f
DESKTOP-PLCE5IA\JIT <test@test.com>
2018-04-05 08:43:14 +0200

    init

---
+++ PlacingBeacons/PlacingBeacons/Pages/PlacingBeaconsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PlacingBeacons
+{
+    public partial class PlacingBeaconsPage : ContentPage
+    {
+        public PlacingBeaconsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
